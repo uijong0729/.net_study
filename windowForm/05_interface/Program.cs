e2c7fec57fdccae438925156bc71e03d0236dbd8
uijong <uijong0729@naver.com>
2018-09-12 15:51:48 +0900

    Grammer Final

---
+++ windowForm/05_interface/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @interface
+{
+    interface IA
+    {
+        void PrintA();
+    }
+
+    interface IB
+    {
+        void PrintB();
+    }
+
+    class C : IA, IB
+    {
+        public void PrintA()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintB()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
